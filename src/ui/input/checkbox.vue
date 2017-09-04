d3ba20093eb4702043251610a82f2b23d009e11f
Tassos Koutsovassilis <t.koutsovassilis@gmail.com>
2017-09-04 22:05:26 +0300

    Added missing default value

--- src/ui/input/checkbox.vue
+++ src/ui/input/checkbox.vue
@@ -23,7 +23,10 @@ export default {
   },
   props: {
     value: null,
-    groupValue: [Array, Boolean]
+    groupValue: {
+      type: [Array, Boolean],
+      default: false
+    }
   },
   computed: {
     isChecked () {
